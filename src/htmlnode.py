90941e166dbb33e25e4939236d5aec7adde6b3f1
kaydarovv <sanzharkaidarovv@gmail.com>
2025-01-17 14:27:39 +0500

    Add parent node logic and html converting

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,13 +15,35 @@ class HTMLNode():
         if not self.props:
             return ""
         
-        result = " ".join(map(lambda element: f" {element[0]}={element[1]}",self.props.items()))
+        result = "".join(map(lambda element: f' {element[0]}="{element[1]}"',self.props.items()))
         return result
     
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str,  children: list, props: dict=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+    
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Missing tag")
+            
+        if not self.children:
+            raise ValueError("WHERE ARE THE CHILDREN, YOU MONSTER")
+        
+        content = ''.join(child.to_html() for child in self.children)
+        result = f"<{self.tag}{self.props_to_html()}>{content}</{self.tag}>" 
+        return result
+
 class LeafNode(HTMLNode):
     def __init__(self, tag: str, value: str, props: dict=None):
         super().__init__(tag=tag, value=value, children=None, props=props)
     
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
     def to_html(self):
         if not self.value:
             raise ValueError("value can't be empty")
@@ -30,3 +52,5 @@ class LeafNode(HTMLNode):
         
         result = f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
         return result
+    
+    
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode
+def text_node_to_html_node(text_node):
 
 def main():
     node1 = TextNode("bananza", TextType.CODE, 'https://www.boot.dev/lessons/cdae7fca-a7dc-4706-b2c5-7a03d66db1c9')
